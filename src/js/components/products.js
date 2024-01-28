031be818a73afad5d37a2a3a432727fea8fdae27
Jakub Stelmasiak <stelmon3@gmail.com>
2024-01-28 22:55:27 +0100

    add nav

--- src/js/components/products.js
+++ src/js/components/products.js
@@ -1,8 +1,8 @@
 
-class Product {
-    constructor(id, data){
-        this.id = id;
-        this.data = data;
+// class Product {
+//   constructor(id, data){
+//     this.id = id;
+//     this.data = data;
         
-    }
-}
+//   }
+// }
