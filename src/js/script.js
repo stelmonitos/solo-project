5eb9e8d7b4e29ca514105d5feb06316e8f018b64
Jakub Stelmasiak <stelmon3@gmail.com>
2024-01-30 13:26:11 +0100

    add initData

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,24 @@
-const app = {
-  init: function() {
+import {settings} from './settings.js';
 
+const app = {
+
+  initData: function(){
+    this.data = {};
+
+    const url = settings.db.url + '/' + settings.db.products;
+    
+    fetch(url)
+      .then(function(rawResponse){
+        return rawResponse.json();
+      })
+      .then(function(parsedResponse){
+        console.log('parsedResponse', parsedResponse);
+      });
+  },
+
+
+  init: function() {
+    this.initData();
   },
 };
 
